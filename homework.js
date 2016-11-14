1349dbfa9393935ae80f850a9148d1701b97b35c
syurakshynairyna <iryna.s@trip-top.com>
2016-11-15 01:46:03 +0200

    Add files via upload

---
+++ homework.js
@@ -0,0 +1,47 @@
+//JasmineJS 2
+
+let URL = 'http://maxcabrera.com/code/todo-list/'
+
+describe('TODO site', function () {
+   
+   it('should have requested title', function () {
+       browser.get(URL)
+       expect(browser.getTitle()).toBe('Angular2 Seed')
+   })
+
+   it('should create new note', function () {
+       browser.get(URL)
+       let webelement = $('input.enter-todo')
+       webelement.sendKeys('Buy weimaraner')
+       webelement.submit()
+       browser.sleep(100)
+
+       webelement.sendKeys('Travel to Tokyo')
+       webelement.submit()
+       browser.sleep(100)
+
+       webelement.sendKeys('Get to know Doctor Strange')
+       webelement.submit()
+       browser.sleep(100)
+
+       let notes = $$('todo-list .small-12')
+
+       //expect(notes.getText()).not.toContain('New Shiny note')expect(notes.g
+       expect(notes.getText()).toContain('Buy weimaraner', 'Travel to Tokyo', 'Get to know Doctor Strange')
+   })
+
+   it('should delete all requested notes', function () {
+    browser.get(URL)
+    for (let i = 1; i <= 7; i++ ) {
+
+     let checkbox = $(‘input.enter-todo’) body > app > todo-list > div > div:nth-child(1) > todo-list-render > div > input[type=”checkbox”]’)
+    checkbox.click();
+    browser.sleep(50)
+
+       expect(notes.getText()).not.toContain('Buy weimaraner', 'Travel to Tokyo', 'Get to know Doctor Strange');
+       expect(notes.isDisplayed()).toBe(false);
+
+   }
+    )
+
+})
